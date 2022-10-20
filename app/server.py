543c8a125cc14f13c98bfdfbf329d871eacf26f6
Brayan Pérez Cobos <brapercob@alum.us.es>
2022-10-20 16:38:34 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -11,10 +11,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/w4khx34p1kqpk1t/model-resnet18.pkl?dl=1'
-export_file_name = 'model-resnet18.pkl'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_name = 'export.pkl'
 
-classes = ['Positivo', 'Negativo']
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
